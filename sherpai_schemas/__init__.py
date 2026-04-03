e9a04b1ea5b55d6cdc8016ccaf266816f5e6dbcf
rklinghammer <roman3533@googlemail.com>
2026-04-03 17:19:59 +0200

    feat: Add logging

--- sherpai_schemas/__init__.py
+++ sherpai_schemas/__init__.py
@@ -1,6 +1,6 @@
 from .schemas import ProblemInstance, ProblemID, Fix, SolutionInstance, MetaDataInstance, Prompts, FormattingRules
 from .functions import parse_dimensions_from_str, parse_dimensions_to_str, get_pure_data, smart_cast
-from .llm_interface import inference_conversation, batch_vectorization, inference_completion, batch_inference_address_extraction, batch_inference_klassifik
+from .llm_interface import inference_conversation, batch_vectorization, inference_completion, batch_inference_address_extraction, batch_inference_klassifik, batch_inference_fix_formatting
 from .vectordb_interface import vectorize_data, query_db
 
 __all__ = [
@@ -22,6 +22,7 @@ __all__ = [
     "batch_vectorization",
     "batch_inference_address_extraction",
     "batch_inference_klassifik",
+    "batch_inference_fix_formatting",
 
     "vectorize_data",
     "query_db",
